8af61ce670305b3d15a7ed428b478c1e37a4b040
khushpatel00 <patelxkhush@gmail.com>
2025-04-17 17:45:30 +0530

    var declaration

---
+++ C++/varDeclaration.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 25;
+    cout<<"Rom Rom BhaiL0g ";
+    cout<<"\n"<<num<<"\n\nmoic"; 
+    char singleChar = '*';
+    cout<<"\n\n"<<singleChar;
+    string sent1 = "Context Message";
+    cout<<"\n\n"<<sent1;
+    float floatCheck = 10000.00001;
+    cout<<"\n\n"<<floatCheck;
+    double doubleCheck = 100000.00001;
+    cout<<"\n\n"<<(double)doubleCheck;
+    char charToNum = 'a';
+    cout<<"\n\ndigit? > "<<(float)charToNum;
+    int numToChar = 98;
+    cout<<"\nCharacter? > "<<(char)numToChar;
+    cin>>num;
+}
