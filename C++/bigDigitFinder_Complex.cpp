2ed983e1d65642e0e918e5fa59d2a23fe853c917
khushpatel00 <patelxkhush@gmail.com>
2025-04-26 20:17:16 +0530

    ladderIfElse_bigDigitFinder

--- C++/bigDigitFinder_Complex.cpp
+++ C++/bigDigitFinder_Complex.cpp
@@ -4,41 +4,19 @@ using namespace std;
 int main(){
     int a,b,c;
 
-    cout<<"Enter value of A : ";
+    cout<<"Enter Value A: ";
     cin>>a;
-    cout<<"Enter value of B : ";
+    cout<<"Enter Value B: ";
     cin>>b;
-    cout<<"Enter value of C : ";
+    cout<<"Enter Value C: ";
     cin>>c;
 
-
-    // SAME PROGRAM WITH EASIER STEPS TO UNDERSTAND
-
-
-    // if(a > b){
-    //     if(a > c){
-    //         cout<<"A is Max\n";
-    //     }
-    //     else{
-    //         cout<<"C is Max\n";
-    //     }
-    // }
-    // else{
-    //     if(b > c){
-    //         cout<<"B is Max\n";
-    //     }
-    //     else{
-    //         cout<<"C is Max\n";
-    //     }
-    // }
-
-
-    // TERNERY OPERATOR (COMPLEX)
-
-
-    a>b ? a>c ? cout<<"A is Max" : cout<<"C is Max" : b>c ? cout<<"B is Max" : cout<<"C is Max";
-
- 
-
+    if(a==b && b==c){cout<<"All are Same !!";}
+    else if(a==b){cout<<"A & B are Same";}
+    else if(b==c){cout<<"B & C are Same";}
+    else if(a==c){cout<<"A & C are Same";}
+    else if(a>b && a>c){cout<<"A is Max";}
+    else if(b>a && b>c){cout<<"B is Max";}
+    else if(c>a && c>b){cout<<"C is Max";}
 
 }
