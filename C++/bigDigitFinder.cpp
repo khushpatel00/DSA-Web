ba80c845e831abb32846360344b2d055cf9cc08b
khushpatel00 <patelxkhush@gmail.com>
2025-04-26 20:07:09 +0530

    finding larger integer among 3 variables

---
+++ C++/bigDigitFinder.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+
+    cout<<"Enter value of A : ";
+    cin>>a;
+    cout<<"Enter value of B : ";
+    cin>>b;
+    cout<<"Enter value of C : ";
+    cin>>c;
+
+    if(a > b){
+        if(a > c){
+            cout<<"A is Max\n";
+        }
+        else{
+            cout<<"C is Max\n";
+        }
+    }
+    else{
+        if(b > c){
+            cout<<"B is Max\n";
+        }
+        else{
+            cout<<"C is Max\n";
+        }
+    }
+}
