dcc9b395a7a3dbd508e964959852c41ac9054f32
khushpatel00 <patelxkhush@gmail.com>
2025-04-29 22:44:05 +0530

    seriesSum

--- C++/4. Loops/seriesOfSum_Loop.cpp
+++ C++/4. Loops/seriesOfSum_Loop.cpp
@@ -2,16 +2,12 @@
 using namespace std;
 
 int main(){
-    int n,i,sum = 1;
+    int sum=0,n,i;
     cout<<"Enter the number to make the sum in series : ";
     cin>>n;
-    // cout<<"enter the initial value to start the sum from (default : 1) : ";
-    // cin>>sum;
 
     for(i=1;i<=n;i++){
-        // cout<<"1. "<<(sum-sum/2)<<" + "<<sum<<" = "<<(sum+=sum)<<endl;
-        cout<<"sum = "<<sum<<endl;
-        sum+=i; 
+        sum+=i;
+        cout<<sum<<endl;
     }
-    cout<<"\n\n\n=================== end of code ======================\n\n";
 }
