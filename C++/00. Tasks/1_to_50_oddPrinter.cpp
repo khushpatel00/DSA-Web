2ee1088123aba3b798b503e50f7ea98a2afb2404
khushpatel00 <patelxkhush@gmail.com>
2025-04-30 19:40:29 +0530

    tasks + comments-Added

--- C++/00. Tasks/1_to_50_oddPrinter.cpp
+++ C++/00. Tasks/1_to_50_oddPrinter.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 int main()
 {
-    int i, n;
-    cout << "entre the no. of odd digits to print: ";
-    cin >> n;
-    for (i = 1; i <= n; i++)
+    int i;
+   
+    for (i = 1; i <= 50; i++)
     {
         if (i % 2) // or (i % 2 = 0)
         {
