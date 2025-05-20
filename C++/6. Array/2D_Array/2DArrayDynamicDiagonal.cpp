e889e87132435b8725821432e4c04cf4d819b4a4
Khush Patel <patelxkhush@gmail.com>
2025-05-20 11:10:42 +0530

    2D array exercise

---
+++ C++/6. Array/2D_Array/2DArrayDynamicDiagonal.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j, row, col;
+    cout << "Enter the number of rows: ";
+    cin >> row;
+    cout << "Enter the number of columns: ";
+    cin >> col;
+    int arr[row][col];
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            cout << "a[" << i << "][" << j << "] = ";
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << endl
+         << endl;
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Diagonal of matrix (array)" << endl;
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            if (i == j) cout << arr[i][j] << " ";
+
+            else cout << "  ";
+        }
+        cout<<endl;
+    }
+}
